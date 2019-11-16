49c9cfd5966bb35b8096d0dac9abe527f1ad9f2c
Thanatossan <kotokikung@gmail.com>
2019-11-16 15:20:43 +0700

    add title to unilogo

--- components/about/univer_list.vue
+++ components/about/univer_list.vue
@@ -8,92 +8,114 @@
         <img
           src="~/assets/uni_logo/1.png"
           id="uniImage"
+          title="คณะวิศวกรรมศาสตร์ มหาวิทยาลัยเชียงใหม่"
         >
         <img
           src="~/assets/uni_logo/2.png"
           id="uniImage"
+          title="คณะวิศวกรรมศาสตร์ มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี"
         >
         <img
           src="~/assets/uni_logo/3.png"
           id="uniImage"
+          title="คณะวิศวกรรมศาสตร์ มหาวิทยาลัยนเรศวร"
         >
         <img
           src="~/assets/uni_logo/4.png"
           id="uniImage"
+          title="คณะวิศวกรรมศาสตร์และอุตสาหกรรมเกษตร มหาวิทยาลัยแม่โจ้"
         >
         <img
           src="~/assets/uni_logo/5.png"
           id="uniImage"
+          title="คณะวิศวกรรมศาสตร์ มหาวิทยาลัยศรีปทุม"
         >
         <img
           src="~/assets/uni_logo/6.png"
           id="uniImage"
+          title="คณะวิศวกรรมศาสตร์ มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ"
         >
         <img
           src="~/assets/uni_logo/7.png"
           id="uniImage"
+          title="คณะวิศวกรรมศาสตร์ มหาวิทยาลัยเทคโนโลยีมหานคร"
         >
 
         <img
           src="~/assets/uni_logo/10.png"
           id="uniImage"
+          title="คณะวิศวกรรมศาสตร์ มหาวิทยาลัยขอนแก่น"
         >
         <img
           src="~/assets/uni_logo/11.png"
           id="uniImage"
+          title="คณะวิศวกรรมศาสตร์ สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง"
         >
         <img
           src="~/assets/uni_logo/12.png"
           id="uniImage"
+          title="คณะวิศวกรรมศาสตร์ มหาวิทยาลัยอุบลราชธานี"
         >
         <img
           src="~/assets/uni_logo/13.png"
           id="uniImage"
+          title="คณะวิศวกรรมศาสตร์ มหาวิทยาลัยมหาสารคาม"
         >
         <img
           src="~/assets/uni_logo/14.png"
           id="uniImage"
+          title="คณะวิศวกรรมศาสตร์ มหาวิทยาลัยบูรพา"
         >
         <img
           src="~/assets/uni_logo/15.png"
           id="uniImage"
+          title="คณะวิศวกรรมศาสตร์ มหาวิทยาลัยสงขลานครินทร์"
         >
         <img
           src="~/assets/uni_logo/16.png"
           id="uniImage"
+          title="คณะวิศวกรรมศาสตร์ มหาวิทยาลัยมหิดล"
         >
         <img
           src="~/assets/uni_logo/17.png"
           id="uniImage"
+          title="คณะวิศวกรรมศาสตร์และเทคโนโลยีอุตสาหกรรม มหาวิทยาลัยศิลปากร"
         >
         <img
           src="~/assets/uni_logo/21.png"
           id="uniImage"
+          title="คณะวิศวกรรมศาสตร์ มหาวิทยาลัยพะเยา"
         >
         <img
           src="~/assets/uni_logo/22.png"
           id="uniImage"
+          title="คณะวิศวกรรมศาสตร์และเทคโนโลยีอุตสาหกรรม มหาวิทยาลัยราชภัฏเพชรบุรี"
         >
 
         <img
           src="~/assets/uni_logo/19.png"
           id="uniImage2"
+          title="คณะวิศวกรรมศาสตร์ มหาวิทยาลัยเทคโนโลยีราชมงคลล้านนา"
         >
         <img
           src="~/assets/uni_logo/20.png"
           id="uniImage2"
+          title=""
         >
         <img
           src="~/assets/uni_logo/8.png"
           id="uniImage2"
+          title="คณะวิศวกรรมศาสตร์ มหาวิทยาลัยเทคโนโลยีราชมงคลธัญบุรี"
         >
         <img
           src="~/assets/uni_logo/9.png"
           id="uniImage2"
+          title="คณะวิศวกรรมศาสตร์ มหาวิทยาลัยเทคโนโลยีราชมงคลพระนคร"
         >
         <img
           src="~/assets/uni_logo/18.png"
           id="uniImage2"
+          title="คณะวิศวกรรมศาสตร์มหาวิทยาลัยเทคโนโลยีราชมงคล รัตนโกสินทร์"
         >
       </div>
     </div>
