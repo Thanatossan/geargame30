6a4f1bab0e8793af966b3ae75b814def4892c7c3
chaiyr <chaiy.rungsiyakull@cmu.ac.th>
2019-11-17 10:46:26 +0700

    Fix Typo

--- components/promote.vue
+++ components/promote.vue
@@ -6,7 +6,7 @@
       </div>
     </div>
     <div class="w-1/2 pl-6 ">
-      <p class="text-left"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ยินดีต้อนรับ นักกีฬาและบุคคลากรทุกท่าน สู่ระบบลงทะเบียน
+      <p class="text-left"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ยินดีต้อนรับ นักกีฬา คณาจารย์และบุคคลากรทุกท่าน สู่ระบบลงทะเบียน
         จ๊างเหล็กเกมส์ กีฬาเกียร์ครั้งที่ 30 ทุกท่านสามารถลงทะเบียน เพื่อ
         กรอกข้อมูลส่วนตัว สำหรับผู้เข้าร่วมกิจกรรม
         ได้ที่เว็บลงทะเบียน โดยผ่านลิ้งค์ด้านล่าง </p>
@@ -14,7 +14,7 @@
       <button
         class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full "
         onclick="window.location = 'https://geargames30.eng.cmu.ac.th/login'"
-      >เข้าสู่ระบบกรอกข้อมูส่วนตัว </button>
+      >เข้าสู่ระบบกรอกข้อมูลส่วนตัว </button>
     </div>
   </div>
 </template>
