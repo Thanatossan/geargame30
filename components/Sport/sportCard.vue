16554e14896ab7e03b186814b4337a25a542b8e6
Thanatossan <kotokikung@gmail.com>
2019-12-19 15:34:37 +0700

    fix img

---
+++ components/Sport/sportCard.vue
@@ -0,0 +1,208 @@
+<template>
+  <section>
+    <div class="row">
+      <div class="col-sm-3">
+        <nuxt-link to="sportDetails">
+          <div class="card-body card-border m-3 btn btn-outline-danger">
+            <img src="~/assets/image/JhangLekGames-Logo.png" />
+            <br />
+            <border />
+            <br />
+            <label class="default-text">ฟุตซอล</label>
+          </div>
+        </nuxt-link>
+      </div>
+      <div class="col-sm-3">
+        <nuxt-link to="sportDetails">
+          <div class="card-body card-border m-3 btn btn-outline-danger">
+            <img src="~/assets/image/JhangLekGames-Logo.png" />
+            <br />
+            <border />
+            <br />
+            <label class="default-text">แบตมินตัน</label>
+          </div>
+        </nuxt-link>
+      </div>
+      <div class="col-sm-3">
+        <nuxt-link to="sportDetails">
+          <div class="card-body card-border m-3 btn btn-outline-danger">
+            <img src="~/assets/image/JhangLekGames-Logo.png" />
+            <br />
+            <border />
+            <br />
+            <label class="default-text">กรีฑา</label>
+          </div>
+        </nuxt-link>
+      </div>
+      <div class="col-sm-3">
+        <nuxt-link to="sportDetails">
+          <div class="card-body card-border m-3 btn btn-outline-danger">
+            <img src="~/assets/image/JhangLekGames-Logo.png" />
+            <br />
+            <border />
+            <br />
+            <label class="default-text">บาสเก็ตบอล</label>
+          </div>
+        </nuxt-link>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-3">
+        <nuxt-link to="sportDetails">
+          <div class="card-body card-border m-3 btn btn-outline-danger">
+            <img src="~/assets/image/JhangLekGames-Logo.png" />
+            <br />
+            <border />
+            <br />
+            <label class="default-text">E-sport</label>
+          </div>
+        </nuxt-link>
+      </div>
+      <div class="col-sm-3">
+        <nuxt-link to="sportDetails">
+          <div class="card-body card-border m-3 btn btn-outline-danger">
+            <img src="~/assets/image/JhangLekGames-Logo.png" />
+            <br />
+            <border />
+            <br />
+            <label class="default-text">กีฬาทางน้ำ</label>
+          </div>
+        </nuxt-link>
+      </div>
+      <div class="col-sm-3">
+        <nuxt-link to="sportDetails">
+          <div class="card-body card-border m-3 btn btn-outline-danger">
+            <img src="~/assets/image/JhangLekGames-Logo.png" />
+            <br />
+            <border />
+            <br />
+            <label class="default-text">ปิงปอง</label>
+          </div>
+        </nuxt-link>
+      </div>
+      <div class="col-sm-3">
+        <nuxt-link to="sportDetails">
+          <div class="card-body card-border m-3 btn btn-outline-danger">
+            <img src="~/assets/image/JhangLekGames-Logo.png" />
+            <br />
+            <border />
+            <br />
+            <label class="default-text">หมากกระดาน</label>
+          </div>
+        </nuxt-link>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-3">
+        <nuxt-link to="sportDetails">
+          <div class="card-body card-border m-3 btn btn-outline-danger">
+            <img src="~/assets/image/JhangLekGames-Logo.png" />
+            <br />
+            <border />
+            <br />
+            <label class="default-text">วอลเล่ย์บอล</label>
+          </div>
+        </nuxt-link>
+      </div>
+      <div class="col-sm-3">
+        <nuxt-link to="sportDetails">
+          <div class="card-body card-border m-3 btn btn-outline-danger">
+            <img src="~/assets/image/JhangLekGames-Logo.png" />
+            <br />
+            <border />
+            <br />
+            <label class="default-text">ฟุตบอล</label>
+          </div>
+        </nuxt-link>
+      </div>
+      <div class="col-sm-3">
+        <nuxt-link to="sportDetails">
+          <div class="card-body card-border m-3 btn btn-outline-danger">
+            <img src="~/assets/image/JhangLekGames-Logo.png" />
+            <br />
+            <border />
+            <br />
+            <label class="default-text">ตะกร้อ</label>
+          </div>
+        </nuxt-link>
+      </div>
+      <div class="col-sm-3">
+        <nuxt-link to="sportDetails">
+          <div class="card-body card-border m-3 btn btn-outline-danger">
+            <img src="~/assets/image/JhangLekGames-Logo.png" />
+            <br />
+            <border />
+            <br />
+            <label class="default-text">ซอฟบอล</label>
+          </div>
+        </nuxt-link>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-3">
+        <nuxt-link to="sportDetails">
+          <div class="card-body card-border m-3 btn btn-outline-danger">
+            <img src="~/assets/image/JhangLekGames-Logo.png" />
+            <br />
+            <border />
+            <br />
+            <label class="default-text">เทนนิส</label>
+          </div>
+        </nuxt-link>
+      </div>
+      <div class="col-sm-3">
+        <nuxt-link to="sportDetails">
+          <div class="card-body card-border m-3 btn btn-outline-danger">
+            <img src="~/assets/image/JhangLekGames-Logo.png" />
+            <br />
+            <border />
+            <br />
+            <label class="default-text">เปตอง</label>
+          </div>
+        </nuxt-link>
+      </div>
+      <div class="col-sm-3">
+        <nuxt-link to="sportDetails">
+          <div class="card-body card-border m-3 btn btn-outline-danger">
+            <img src="~/assets/image/JhangLekGames-Logo.png" />
+            <br />
+            <border />
+            <br />
+            <label class="default-text">รักบี้</label>
+          </div>
+        </nuxt-link>
+      </div>
+      <div class="col-sm-3">
+        <nuxt-link to="sportDetails">
+          <div class="card-body card-border m-3 btn btn-outline-danger">
+            <img src="~/assets/image/JhangLekGames-Logo.png" />
+            <br />
+            <border />
+            <br />
+            <label class="default-text">sport</label>
+          </div>
+        </nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import border from "~/components/border";
+export default {
+  components: {
+    border
+  }
+};
+</script>
+<style scoped>
+.card-border {
+  border: 2px solid rgb(214, 212, 212);
+  border-radius: 15px;
+  display: block;
+}
+.default-text {
+  font-family: Rubik;
+  text-align: center;
+  font-size: 20px;
+}
+</style>
---
+++ pages/sport.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <link
+      href="https://fonts.googleapis.com/css?family=Rubik&display=swap"
+      rel="stylesheet"
+    />
+    <div class="container min-h-screen flex justify-center text-center mx-auto">
+      <div class="w-5/6">
+        <navbar />
+        <!-- title section -->
+        <div
+          class="flex flex-col justify-center mb-4 bg-red-800 text-center lg:h-48 w-full"
+        >
+          <label class="default-text" style="font-size:55px; color:white;"
+            >SPORTS</label
+          >
+        </div>
+        <!-- content section -->
+        <sportCon />
+      </div>
+    </div>
+
+    <br />
+
+    <!-- footer -->
+    <br />
+    <border />
+    <div class="container flex justify-center items-center text-center mx-auto">
+      <div class="w-4/6">
+        <div class="my-5 mx-auto text-right">
+          <Footer />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import navbar from "~/components/navbar";
+import border from "~/components/border";
+import sportCon from "~/components/Sport/sportCard";
+import Footer from "~/components/Footer";
+export default {
+  components: {
+    navbar,
+    border,
+    Footer,
+    sportCon
+  }
+};
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS */
+.default-text {
+  font-family: Rubik;
+}
+</style>
