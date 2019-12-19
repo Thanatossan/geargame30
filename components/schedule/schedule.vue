a3563769ef747f3227307cf58a1ff7716676d118
LazyP <35336893+ghinida@users.noreply.github.com>
2019-12-19 22:53:50 +0700

    responsive Schedule fixed
    fixed responsive Schedule page

--- components/schedule/schedule.vue
+++ components/schedule/schedule.vue
@@ -2,148 +2,220 @@
   <div class="container ml-4">
     <h2 class="text-left text-5xl"> Schedule</h2>
     <border />
-    <br>
     <h2 class="bg-gray-300 text-left text-2xl p-2"> Monday 23 December</h2>
-    <div class="flex text-xl">
-      <div class="w-1/3">
-        <h2 class="mt-3"> 8:00 น. </h2>
-        <h2 class="mt-3"> 20:00 น.</h2>
-
-      </div>
-      <div class="w-2/3 text-left">
-        <h2 class="mt-3"> ลงทะเบียนนักกีฬา ณ ศาลาอ่างแก้ว </h2>
-        <h2 class="mt-3"> ปิดรับการลงทะเบียน </h2>
-      </div>
-    </div>
+    <table class="table-fixed w-full">
+      <tbody class="text-xl">
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 8:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> ลงทะเบียนนักกีฬา ณ ศาลาอ่างแก้ว </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 20:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> ปิดรับการลงทะเบียน </h2></td>
+        </tr>
+      </tbody>
+    </table>
     <br>
+    
     <h2 class="bg-gray-300 text-left text-2xl p-2"> Tuesday 24 December</h2>
-    <div class="flex text-xl">
-      <div class="w-1/3">
-        <h2 class="mt-3"> 8:00 น. </h2>
-        <h2 class="mt-3"> 12.00 น.</h2>
-        <h2 class="mt-3"> 13:00 น. </h2>
-        <h2 class="mt-3"> 15.00 น.</h2>
-        <h2 class="mt-3"> 16:00 น. </h2>
-        <h2 class="mt-3"> 17.00 น.</h2>
-        <h2 class="mt-3"> 18:00 น. </h2>
-        <h2 class="mt-3"> 18.05 น.</h2>
-        <h2 class="mt-3"> 18:20 น. </h2>
-        <h2 class="mt-3"> 18.35 น.</h2>
-        <h2 class="mt-3"> 18:40 น. </h2>
-        <h2 class="mt-3"> 18.55 น.</h2>
-        <h2 class="mt-3"> 19:05 น. </h2>
-        <h2 class="mt-3"> 19.35 น.</h2>
-        <h2 class="mt-3"> 19.40 น.</h2>
+    <table class="table-fixed w-full">
+      <tbody class="text-xl">
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 8:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> แข่งขันกีฬา </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 12.00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> พักทานอาหารกลางวัน </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 13:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> แข่งขันกีฬา(ต่อ) </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 15.00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> พักการแข่งขันกีฬาเพื่อเข้าร่วมพิธีเปิด </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 16:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> ผู้ร่วมงานเตรียมเดินขบวน ณ สนามกีฬากลาง มหาวิทยาลัยเชียงใหม่ </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 17.00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> เดินขบวนเข้าสู่สนามกีฬา </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 18:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> เคารพธงชาติ </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 18.05 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> กล่าวรายงานประธานในพิธี </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 18:20 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> ประธานในพิธีกล่าวเปิด </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 18.35 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> ผู้นำนักกีฬาจุดคบเพลิง </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 18:40 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> เชิญธงกีฬาขึ้นสู่ยอดเสา </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 18.55 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> ผู้นำนักกีฬากล่าวคำปฏิญาณ </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 19:05 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> การแสดงเฉลิมฉลอง “ครบรอบ 50 ปี วิศวะฯ มช.” </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 19.35 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> ร้องเพลง”แรงเลือดหมู” </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 19.40 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> เดินทางกลับที่พัก </h2></td>
+        </tr>
 
-      </div>
-      <div class="w-2/3 text-left">
-        <h2 class="mt-3"> แข่งขันกีฬา</h2>
-        <h2 class="mt-3"> พักทานอาหารกลางวัน </h2>
-        <h2 class="mt-3"> แข่งขันกีฬา(ต่อ)</h2>
-        <h2 class="mt-3"> พักการแข่งขันกีฬาเพื่อเข้าร่วมพิธีเปิด </h2>
-        <h2 class="mt-3"> ผู้ร่วมงานเตรียมเดินขบวน ณ สนามกีฬากลาง มหาวิทยาลัยเชียงใหม่</h2>
-        <h2 class="mt-3"> เดินขบวนเข้าสู่สนามกีฬา </h2>
-        <h2 class="mt-3"> เคารพธงชาติ</h2>
-        <h2 class="mt-3"> กล่าวรายงานประธานในพิธี </h2>
-        <h2 class="mt-3"> ประธานในพิธีกล่าวเปิด</h2>
-        <h2 class="mt-3"> ผู้นำนักกีฬาจุดคบเพลิง </h2>
-        <h2 class="mt-3"> เชิญธงกีฬาขึ้นสู่ยอดเสา</h2>
-        <h2 class="mt-3"> ผู้นำนักกีฬากล่าวคำปฏิญาณ </h2>
-        <h2 class="mt-3"> การแสดงเฉลิมฉลอง “ครบรอบ 50 ปี วิศวะฯ มช.”</h2>
-        <h2 class="mt-3"> ร้องเพลง”แรงเลือดหมู” </h2>
-        <h2 class="mt-3"> เดินทางกลับที่พัก </h2>
-      </div>
-    </div>
+      </tbody>
+    </table>
     <br>
     <h2 class="bg-gray-300 text-left text-2xl p-2"> Wednesday 25 December</h2>
-    <div class="flex text-xl">
-      <div class="w-1/3">
-        <h2 class="mt-3"> 8:00 น. </h2>
-        <h2 class="mt-3"> 12:00 น. </h2>
-        <h2 class="mt-3"> 13:00 น. </h2>
-        <h2 class="mt-3"> 17:00 น. </h2>
-        <h2 class="mt-3"> 18:00 น. </h2>
-        <h2 class="mt-3"> 21:30 น.</h2>
-
-      </div>
-      <div class="w-2/3 text-left">
-        <h2 class="mt-3"> แข่งขันกีฬา</h2>
-        <h2 class="mt-3"> พักรับประทานอาหารกลางวัน </h2>
-        <h2 class="mt-3"> แข่งขันกีฬา(ต่อ)</h2>
-        <h2 class="mt-3"> พักรับประทานอาหารเย็น </h2>
-        <h2 class="mt-3"> แข่งขันกีฬา(ต่อ)</h2>
-        <h2 class="mt-3"> สิ้นสุดการแข่งขันกีฬาประจำวัน </h2>
-      </div>
-    </div>
+    <table class="table-fixed w-full">
+      <tbody class="text-xl">
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 8:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> แข่งขันกีฬา </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 12:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> พักรับประทานอาหารกลางวัน </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 13:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> แข่งขันกีฬา(ต่อ) </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 17:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> พักรับประทานอาหารเย็น </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 18:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> แข่งขันกีฬา(ต่อ) </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 21:30 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> สิ้นสุดการแข่งขันกีฬาประจำวัน </h2></td>
+        </tr>
+      </tbody>
+    </table>
     <br>
     <h2 class="bg-gray-300 text-left text-2xl p-2"> Thusday 26 December</h2>
-    <div class="flex text-xl">
-      <div class="w-1/3">
-        <h2 class="mt-3"> 8:00 น. </h2>
-        <h2 class="mt-3"> 12:00 น. </h2>
-        <h2 class="mt-3"> 13:00 น. </h2>
-        <h2 class="mt-3"> 16:00 น. </h2>
-        <h2 class="mt-3"> 16:15 น. </h2>
-        <h2 class="mt-3"> 16:30 น.</h2>
-        <h2 class="mt-3"> 21:30 น.</h2>
-        <h2 class="mt-3"> 22:00 น.</h2>
-      </div>
-      <div class="w-2/3 text-left">
-        <h2 class="mt-3"> แข่งขันกีฬา </h2>
-        <h2 class="mt-3"> พักรับประทานอาหารกลางวัน </h2>
-        <h2 class="mt-3"> แข่งขันกีฬา(ต่อ) </h2>
-        <h2 class="mt-3"> ผู้เข้าร่วมงานแข่งสันทนาการเดินทางมาที่ สนามกีฬากลาง </h2>
-        <h2 class="mt-3"> พิธีกรชี้แจงรายละเอียดการแข่งขันกีฬาฮาเฮ </h2>
-        <h2 class="mt-3"> แข่งขันกีฬาฮาเฮ </h2>
-        <h2 class="mt-3"> สันทนาการรวม </h2>
-        <h2 class="mt-3"> สิ้นสุดการแข่งขันกีฬาประจำวัน </h2>
-      </div>
-    </div>
+    <table class="table-fixed w-full">
+      <tbody class="text-xl">
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 8:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> แข่งขันกีฬา </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 12:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> พักรับประทานอาหารกลางวัน </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 13:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> แข่งขันกีฬา(ต่อ) </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 16:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> ผู้เข้าร่วมงานแข่งสันทนาการเดินทางมาที่ สนามกีฬากลาง </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 16:15 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> พิธีกรชี้แจงรายละเอียดการแข่งขันกีฬาฮาเฮ </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 16:30 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> แข่งขันกีฬาฮาเฮ </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 21:30 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> สันทนาการรวม </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 22:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> สิ้นสุดการแข่งขันกีฬาประจำวัน </h2></td>
+        </tr>
+      </tbody>
+    </table>
     <br>
     <h2 class="bg-gray-300 text-left text-2xl p-2"> Friday 27 December</h2>
-    <div class="flex text-xl">
-      <div class="w-1/3">
-        <h2 class="mt-3"> 8:00 น. </h2>
-        <h2 class="mt-3"> 12:00 น. </h2>
-        <h2 class="mt-3"> 13:00 น. </h2>
-        <h2 class="mt-3"> 14:30 น. </h2>
-        <h2 class="mt-3"> 16:00 น. </h2>
-        <h2 class="mt-3"> 17:00 น.</h2>
-        <h2 class="mt-3"> 17:10 น.</h2>
-        <h2 class="mt-3"> 17:20 น.</h2>
-        <h2 class="mt-3"> 17:30 น.</h2>
-        <h2 class="mt-3"> 20:00 น.</h2>
-        <h2 class="mt-3"> 20:30 น.</h2>
-        <h2 class="mt-3"> 22:00 น.</h2>
-
-      </div>
-      <div class="w-2/3 text-left">
-        <h2 class="mt-3">แข่งขันกีฬา </h2>
-        <h2 class="mt-3">พักรับประทานอาหารกลางวัน </h2>
-        <h2 class="mt-3">แข่งขันกีฬา(ต่อ) </h2>
-        <h2 class="mt-3">สิ้นสุดการแข่งขัน เพื่อเตรียมตัวเข้าร่วมพิธีปิด </h2>
-        <h2 class="mt-3">เริ่มเดินทางเข้าพิธีปิด </h2>
-        <h2 class="mt-3">ผู้นำนักกีฬากล่าวรายงานปิดกิจกรรม</h2>
-        <h2 class="mt-3">ประธานในพิธีกล่าวปิดงาน</h2>
-        <h2 class="mt-3">มอบธงให้เจ้าภาพครั้งต่อไป </h2>
-        <h2 class="mt-3">ประกวดดาว-เดือน </h2>
-        <h2 class="mt-3">การแสดงของนักศึกษาคณะวิศวกรรมศาสตร์ มหาวิทยาลัยเชียงใหม่ </h2>
-        <h2 class="mt-3">การแสดงดนตรีของนักศึกษาคณะวิศวกรรมศาสตร์ มหาวิทยาลัยเชียงใหม่ </h2>
-        <h2 class="mt-3">เดินทางกลับที่พัก </h2>
-
-        <br>
-      </div>
-    </div>
+    <table class="table-fixed w-full">
+      <tbody class="text-xl">
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 8:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> แข่งขันกีฬา </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 12:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> พักรับประทานอาหารกลางวัน </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 13:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> แข่งขันกีฬา(ต่อ) </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 14:30 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> สิ้นสุดการแข่งขัน เพื่อเตรียมตัวเข้าร่วมพิธีปิด </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 16:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> เริ่มเดินทางเข้าพิธีปิด </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 17:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> ผู้นำนักกีฬากล่าวรายงานปิดกิจกรรม </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 17:10 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> ประธานในพิธีกล่าวปิดงาน </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 17:20 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> มอบธงให้เจ้าภาพครั้งต่อไป </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 17:30 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> ประกวดดาว-เดือน </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 20:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> การแสดงของนักศึกษาคณะวิศวกรรมศาสตร์ มหาวิทยาลัยเชียงใหม่ </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 20:30 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> การแสดงดนตรีของนักศึกษาคณะวิศวกรรมศาสตร์ มหาวิทยาลัยเชียงใหม่ </h2></td>
+        </tr>
+        <tr>
+          <td class="w-1/3"><h2 class="text-center mt-3"> 22:00 น. </h2></td>
+          <td class="w-2/3"><h2 class="mt-3"> เดินทางกลับที่พัก </h2></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import border from "~/components/border";
-export default {
-  components: {
-    border
-  }
-};
+  import border from "~/components/border";
+  export default {
+    components: {
+      border
+    }
+  };
+
 </script>
 
 <style>
