ae9fb25f9f175c85b02e2f7b4fad48af3c835347
Thanatossan <kotokikung@gmail.com>
2019-12-21 04:42:03 +0700

    change nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,7 @@ module.exports = {
     extend(config, ctx) {}
   },
   server: {
-    port: 3000, // default: 3000
+    port: 8000, // default: 3000
     host: "0.0.0.0" // default: localhost
   }
 };
