54dfbfda69982b586d3837c216ef41a980456be8
Thanatossan <kotokikung@gmail.com>
2019-12-23 02:54:29 +0700

    fix

--- components/Sport/SponCard.vue
+++ components/Sport/SponCard.vue
@@ -1,35 +1,40 @@
 <template>
   <div class="container flex flex-col lg:flex-row items-stretch">
-    <div
-      class="flex flex-col justify-center flex-1 max-w-sm rounded overflow-hidden w-2/3 lg:w-auto h-2/3 lg:h-auto border-solid border-2 border-gray-600 self-center">
-      <img class="w-full object-contain self-center" src="~/assets/sponcer/content/CMI Online 1280x512px-01.jpg" />
+    <div class="flex flex-col justify-center flex-1 max-w-sm rounded overflow-hidden w-2/3 lg:w-auto h-2/3 lg:h-auto border-solid border-2 border-gray-600 self-center">
+      <img
+        class="w-full object-contain self-center"
+        src="~/assets/sponcer/content/CMI Online 1280x512px-01.jpg"
+      />
       <div class="px-6 self-center">
-        <p class="text-gray-700 text-base">
-          SF
-        </p>
       </div>
     </div>
-    <div
-      class="flex flex-col justify-center flex-1 max-w-sm rounded overflow-hidden w-2/3 lg:w-auto h-2/3 lg:h-auto border-solid border-2 border-gray-600 self-center">
-      <img class="w-full object-contain self-center" src="~/assets/sponcer/content/S__20308142.jpg" />
-      <div class="px-6 self-center">
-        <p class="text-gray-700 text-base">
-
-        </p>
-      </div>
-    </div>
-    <div
-      class="flex flex-col justify-center flex-1 max-w-sm rounded overflow-hidden w-2/3 lg:w-auto h-2/3 lg:h-auto border-solid border-2 border-gray-600 self-center">
-      <img class="w-full object-contain self-center" src="~/assets/sponcer/content/S__20308144.jpg" />
+    <div class="flex flex-col justify-center flex-1 max-w-sm rounded overflow-hidden w-2/3 lg:w-auto h-2/3 lg:h-auto border-solid border-2 border-gray-600 self-center">
+      <img
+        class="w-full object-contain self-center"
+        src="~/assets/sponcer/content/S__20308142.jpg"
+      />
       <div class="px-6 self-center">
         <p class="text-gray-700 text-base">
 
         </p>
       </div>
     </div>
-    <div
-      class="flex flex-col justify-center flex-1 max-w-sm rounded overflow-hidden w-2/3 lg:w-auto h-2/3 lg:h-auto border-solid border-2 border-gray-600 self-center">
-      <img class="w-full object-contain self-center" src="~/assets/sponcer/content/ufare 2563.png" />
+    <div class="flex flex-col justify-center flex-1 max-w-sm rounded overflow-hidden w-2/3 lg:w-auto h-2/3 lg:h-auto border-solid border-2 border-gray-600 self-center">
+      <img
+        class="w-full object-contain self-center"
+        src="~/assets/sponcer/content/S__20308144.jpg"
+      />
+      <div class="px-6 self-center">
+        <p class="text-gray-700 text-base">
+
+        </p>
+      </div>
+    </div>
+    <div class="flex flex-col justify-center flex-1 max-w-sm rounded overflow-hidden w-2/3 lg:w-auto h-2/3 lg:h-auto border-solid border-2 border-gray-600 self-center">
+      <img
+        class="w-full object-contain self-center"
+        src="~/assets/sponcer/content/ufare 2563.png"
+      />
       <div class="px-6 self-center">
         <p class="text-gray-700 text-base">
 
@@ -57,13 +62,12 @@
 </template>
 
 <script>
-  export default {};
+export default {};
 </script>
 
 <style>
-  .flex-1 {
-    margin-left: 1rem;
-    margin-top: 1rem;
-  }
-
+.flex-1 {
+  margin-left: 1rem;
+  margin-top: 1rem;
+}
 </style>
