8650dd59bca8bc3e13cca33cbb38e38c540fdaa4
Thanatossan <kotokikung@gmail.com>
2019-12-24 05:12:27 +0700

    fix img

--- components/Sport/liveScore.vue
+++ components/Sport/liveScore.vue
@@ -10,32 +10,41 @@
       <div id="Live_Score_table">
         <div class="border-b-2 mt-3 mb-8 "></div>
         <!-- V for -->
-        <div class="flex bg-gray-100 rounded-lg p-1 m-3" >
+        <div class="flex bg-gray-100 rounded-lg p-1 m-3">
           <div id="1stimgField">
             <img
-            src="~/assets/image/JhangLekGames-Logo_wb.png"
-            class="lg:object-scale-down max-h-small"
-          />
+              src="~/assets/image/JhangLekGames-Logo_wb.png"
+              class="lg:object-scale-down max-h-small"
+            />
           </div>
-          <h2 class="m-auto" id="1st">Coming Soon</h2>
+          <h2
+            class="m-auto"
+            id="1st"
+          >Coming Soon</h2>
         </div>
         <div class="flex bg-gray-100 rounded-lg p-1 m-3">
           <div id="2ndimgField">
             <img
-            src="~/assets/image/JhangLekGames-Logo_wb.png"
-            class="lg:object-scale-down max-h-small"
-          />
+              src="~/assets/image/JhangLekGames-Logo_wb.png"
+              class="lg:object-scale-down max-h-small"
+            />
           </div>
-          <h2 class="m-auto" id="2nd">Coming Soon</h2>
+          <h2
+            class="m-auto"
+            id="2nd"
+          >Coming Soon</h2>
         </div>
         <div class="flex bg-gray-100 rounded-lg p-1 m-3">
           <div id="3rdimgField">
             <img
-            src="~/assets/image/JhangLekGames-Logo_wb.png"
-            class="lg:object-scale-down max-h-small"
-          />
+              src="~/assets/image/JhangLekGames-Logo_wb.png"
+              class="lg:object-scale-down max-h-small"
+            />
           </div>
-          <h2 class="m-auto" id="3rd">Coming Soon</h2>
+          <h2
+            class="m-auto"
+            id="3rd"
+          >Coming Soon</h2>
         </div>
         <!-- loop end-->
       </div>
@@ -45,26 +54,23 @@
 
 <script>
 import { api } from "../../api/api";
-import $ from 'jquery'
+import $ from "jquery";
 export default {
-
-  data(){
-    return {
-      
-    }
+  data() {
+    return {};
   },
 
-  methods : {
-    async appendliveSocre(arr){
-      var socreOrder = ['#1st','#2nd','#3rd']
-      var imgOrder = ['#1stimgField','#2ndimgField','#3rdimgField']
+  methods: {
+    async appendliveSocre(arr) {
+      var socreOrder = ["#1st", "#2nd", "#3rd"];
+      var imgOrder = ["#1stimgField", "#2ndimgField", "#3rdimgField"];
       let i = 0;
-      var imgTag = ''
+      var imgTag = "";
       arr.forEach(each => {
-        imgTag = this.mapImg(each.sportType)
-        $(imgOrder[i]).html(imgTag)
-        if(each.sportType == 'Swimming'){
-          $(socreOrder[i]).hide('fast')
+        imgTag = this.mapImg(each.sportType);
+        $(imgOrder[i]).html(imgTag);
+        if (each.sportType == "Swimming") {
+          $(socreOrder[i]).hide("fast");
           $(`<div class="m-auto"> <table class="table-fixed "> 
               <thead >
                 <tr> 
@@ -78,20 +84,22 @@ export default {
               <td class="px-4">${each.scoreTeam2}</td>
               <td class="px-4">${each.scoreTeam3}</td>
             </tbody>
-            </table></div>`).insertAfter(imgOrder[i])
+            </table></div>`).insertAfter(imgOrder[i]);
         }
         // else if(each.sportType == 'Athletics'){
-          
+
         // }
-        else{
-          $(socreOrder[i]).html(`<h2 class="m-auto">${each.nameTeam1} ${each.scoreTeam1} - ${each.scoreTeam2} ${each.nameTeam2}</h2>`)
-          $(socreOrder[i]).show('fast')
+        else {
+          $(socreOrder[i]).html(
+            `<h2 class="m-auto">${each.nameTeam1} ${each.scoreTeam1} - ${each.scoreTeam2} ${each.nameTeam2}</h2>`
+          );
+          $(socreOrder[i]).show("fast");
         }
-        i++
+        i++;
       });
     },
-    async threeLastUpdatedScore(arr){
-      var threeLeastScore = []
+    async threeLastUpdatedScore(arr) {
+      var threeLeastScore = [];
       // var sort = arr.sort(
       //   function(a,b){
       //     var keyA = new Date(a.updated)
@@ -100,81 +108,77 @@ export default {
       //     else if(keyA < keyB) return 1;
       //     else return 0;
       // })
-      let len = arr.length
-      let loop
-      if(len >= 3) loop = 3
-      else loop = len
-      for(let i=0 ;i<loop;i++){
+      let len = arr.length;
+      let loop;
+      if (len >= 3) loop = 3;
+      else loop = len;
+      for (let i = 0; i < loop; i++) {
         threeLeastScore.push(arr[i]);
       }
-      return threeLeastScore
+      return threeLeastScore;
     },
-    mapImg(sportType){
-      var imgTag  = '<img src="/_nuxt/img/53a1fad.png" class="lg:object-scale-down max-h-small"/>'
-      switch(sportType){
-        case 'Badminton' :
-          imgTag =  `<img src="/_nuxt/img/241e671.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Basketball' :
-          imgTag =  `<img src="/_nuxt/img/11dd02b.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'E-sport' :
-          imgTag =  `<img src="/_nuxt/img/fd2e01.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Football' :
-          imgTag =  `<img src="/_nuxt/img/33dc3c6.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Futsal' :
-          imgTag =  `<img src="/_nuxt/img/8578cbc.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Petanque' :
-          imgTag =  `<img src="/_nuxt/img/8ad1bed.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Tabletennis' :
-          imgTag =  `<img src="/_nuxt/img/18b415f.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Rugbyfootball' :
-          imgTag =  `<img src="/_nuxt/img/bcb9655.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Athletics' :
-          imgTag =  `<img src="/_nuxt/img/c61456b.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Entertainment' :
-          imgTag =  `<img src="/_nuxt/img/add23ac.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Sepaktakraw' :
-          imgTag =  `<img src="/_nuxt/img/02b4701.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Softball' :
-          imgTag =  `<img src="/_nuxt/img/0857216.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Swimming' :
-          imgTag =  `<img src="/_nuxt/img/cdaf21d.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Tennis' :
-          imgTag =  `<img src="/_nuxt/img/3702de5.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Volleyball' :
-          imgTag =  `<img src="/_nuxt/img/e6754dd.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Boardgame' :
-          imgTag =  `<img src="/_nuxt/img/6261fa6.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
+    mapImg(sportType) {
+      var imgTag =
+        '<img src="/_nuxt/img/53a1fad.png" class="lg:object-scale-down max-h-small"/>';
+      switch (sportType) {
+        case "Badminton":
+          imgTag = `<img src="/_nuxt/img/241e671.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Basketball":
+          imgTag = `<img src="/_nuxt/img/11dd02b.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "E-sport":
+          imgTag = `<img src="/_nuxt/img/fde2e01.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Football":
+          imgTag = `<img src="/_nuxt/img/33dc3c6.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Futsal":
+          imgTag = `<img src="/_nuxt/img/8578cbc.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Petanque":
+          imgTag = `<img src="/_nuxt/img/8ad1bed.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Tabletennis":
+          imgTag = `<img src="/_nuxt/img/18b415f.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Rugbyfootball":
+          imgTag = `<img src="/_nuxt/img/bcb9655.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Athletics":
+          imgTag = `<img src="/_nuxt/img/c61456b.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Entertainment":
+          imgTag = `<img src="/_nuxt/img/add23ac.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Sepaktakraw":
+          imgTag = `<img src="/_nuxt/img/02b4701.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Softball":
+          imgTag = `<img src="/_nuxt/img/0857216.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Swimming":
+          imgTag = `<img src="/_nuxt/img/cdaf21d.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Tennis":
+          imgTag = `<img src="/_nuxt/img/3702de5.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Volleyball":
+          imgTag = `<img src="/_nuxt/img/e6754dd.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Boardgame":
+          imgTag = `<img src="/_nuxt/img/6261fa6.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
       }
-      return imgTag
-    },
-   
+      return imgTag;
+    }
   },
-  async mounted (){
-     const match = await api.get('/api/getMatch')
-     var update = await this.threeLastUpdatedScore(match.data)
-     await this.appendliveSocre(update)
+  async mounted() {
+    const match = await api.get("/api/getMatch");
+    var update = await this.threeLastUpdatedScore(match.data);
+    await this.appendliveSocre(update);
   },
-  computed : {
-     
-    
-  }
-  
+  computed: {}
 };
 </script>
 
--- components/Sport/match.vue
+++ components/Sport/match.vue
@@ -12,30 +12,39 @@
         <div class="flex bg-gray-100 rounded-lg p-1 m-3">
           <div id="1stIMG">
             <img
-            src="~/assets/image/JhangLekGames-Logo_wb.png"
-            class="lg:object-scale-down max-h-small"
-          />
+              src="~/assets/image/JhangLekGames-Logo_wb.png"
+              class="lg:object-scale-down max-h-small"
+            />
           </div>
-          <h2 class="m-auto" id="1stincome">Coming Soon</h2>
+          <h2
+            class="m-auto"
+            id="1stincome"
+          >Coming Soon</h2>
         </div>
 
         <div class="flex bg-gray-100 rounded-lg p-1 m-3">
-           <div id="2ndIMG">
+          <div id="2ndIMG">
             <img
-            src="~/assets/image/JhangLekGames-Logo_wb.png"
-            class="lg:object-scale-down max-h-small"
-          />
+              src="~/assets/image/JhangLekGames-Logo_wb.png"
+              class="lg:object-scale-down max-h-small"
+            />
           </div>
-          <h2 class="m-auto" id="2ndincome">Coming Soon</h2>
+          <h2
+            class="m-auto"
+            id="2ndincome"
+          >Coming Soon</h2>
         </div>
         <div class="flex bg-gray-100 rounded-lg p-1 m-3">
           <div id="3rdIMG">
             <img
-            src="~/assets/image/JhangLekGames-Logo_wb.png"
-            class="lg:object-scale-down max-h-small"
-          />
+              src="~/assets/image/JhangLekGames-Logo_wb.png"
+              class="lg:object-scale-down max-h-small"
+            />
           </div>
-          <h2 class="m-auto" id="3rdincome">Coming Soon</h2>
+          <h2
+            class="m-auto"
+            id="3rdincome"
+          >Coming Soon</h2>
         </div>
       </div>
     </div>
@@ -43,25 +52,24 @@
 </template>
 
 <script>
-import {api} from '../../api/api'
-import $ from 'jquery'
+import { api } from "../../api/api";
+import $ from "jquery";
 export default {
-  data(){
-    return{
-
-    }
+  data() {
+    return {};
   },
-  methods : {
-    async appendincomingMatch(arr){
-      var scoreOrder = ['#1stincome','#2ndincome','#3rdincome']
-      var imgOrder = ['#1stIMG','#2ndIMG','#3rdIMG']
+  methods: {
+    async appendincomingMatch(arr) {
+      var scoreOrder = ["#1stincome", "#2ndincome", "#3rdincome"];
+      var imgOrder = ["#1stIMG", "#2ndIMG", "#3rdIMG"];
       let i = 0;
-      var imgTag = ''
+      var imgTag = "";
+      var date = "";
       arr.forEach(each => {
-        imgTag = this.mapImg(each.sportType)
-        $(imgOrder[i]).html(imgTag)
-        if(each.sportType == 'Swimming'){
-          $(scoreOrder[i]).hide('fast')
+        imgTag = this.mapImg(each.sportType);
+        $(imgOrder[i]).html(imgTag);
+        if (each.sportType == "Swimming") {
+          $(scoreOrder[i]).hide("fast");
           $(`<div class="m-auto"> <table class="table-fixed "> 
               <thead >
                 <tr> 
@@ -70,29 +78,36 @@ export default {
               <th class="px-4">${each.team3}</th>
             </tr> 
             </thead>
-            </table></div>`).insertAfter(imgOrder[i])
+            </table></div>`).insertAfter(imgOrder[i]);
         }
         // else if(each.sportType == 'Athletics'){
-          
+
         // }
-        else{
+        else {
           $(scoreOrder[i]).html(`
-            <h2 class="m-auto">${each.team1} VS ${each.team2}</h2>
-            <h2 class="m-auto">${each.startTime}</h2>
-          `)
-          $(scoreOrder[i]).show('fast')
+            <h2 class="m-auto text-center">${each.team1} VS ${each.team2}</h2>
+            <h2 class="m-auto text-center"> ${each.startTime.substr(0, 10)}</h2>
+            <h2 class="m-auto text-center"> ${each.startTime
+              .substr(1, 15)
+              .substr(10, 16)} - ${each.endTime
+            .substr(1, 15)
+            .substr(10, 16)}</h2>
+            
+            
+          `);
+          $(scoreOrder[i]).show("fast");
         }
-        i++
+        i++;
       });
     },
-    async getIncomingMatch(arr){
-      var returnArr = []
+    async getIncomingMatch(arr) {
+      var returnArr = [];
       arr.forEach(each => {
         let check = Date.parse(each.startTime) - Date.now();
-        if(check > 0 ) {
-          each.temp = Date.parse(each.startTime) - Date.now()
-          returnArr.push(each)
-        } 
+        if (check > 0) {
+          each.temp = Date.parse(each.startTime) - Date.now();
+          returnArr.push(each);
+        }
       });
       // var sort = returnArr.sort(
       //   function(a,b){
@@ -102,78 +117,78 @@ export default {
       //     else if(keyA > keyB) return 1;
       //     else return 0;
       // })
-      let len = arr.length
-      let loop
-      if(len >= 3) loop = 3
-      else loop = len
-      returnArr = []
-      for(let i=0 ;i<loop;i++){
+      let len = arr.length;
+      let loop;
+      if (len >= 3) loop = 3;
+      else loop = len;
+      returnArr = [];
+      for (let i = 0; i < loop; i++) {
         returnArr.push(arr[i]);
       }
-      return returnArr
+      return returnArr;
     },
-    mapImg(sportType){
-      var imgTag  = '<img src="/_nuxt/img/53a1fad.png" class="lg:object-scale-down max-h-small"/>'
-      switch(sportType){
-        case 'Badminton' :
-          imgTag =  `<img src="/_nuxt/img/241e671.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Basketball' :
-          imgTag =  `<img src="/_nuxt/img/11dd02b.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'E-sport' :
-          imgTag =  `<img src="/_nuxt/img/fd2e01.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Football' :
-          imgTag =  `<img src="/_nuxt/img/33dc3c6.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Futsal' :
-          imgTag =  `<img src="/_nuxt/img/8578cbc.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Petanque' :
-          imgTag =  `<img src="/_nuxt/img/8ad1bed.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Tabletennis' :
-          imgTag =  `<img src="/_nuxt/img/18b415f.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Rugbyfootball' :
-          imgTag =  `<img src="/_nuxt/img/bcb9655.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Athletics' :
-          imgTag =  `<img src="/_nuxt/img/c61456b.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Entertainment' :
-          imgTag =  `<img src="/_nuxt/img/add23ac.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Sepaktakraw' :
-          imgTag =  `<img src="/_nuxt/assets/02b4701.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Softball' :
-          imgTag =  `<img src="/_nuxt/img/0857216.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Swimming' :
-          imgTag =  `<img src="/_nuxt/img/cdaf21d.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Tennis' :
-          imgTag =  `<img src="/_nuxt/img/3702de5.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Volleyball' :
-          imgTag =  `<img src="/_nuxt/img/e6754dd.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
-        case 'Boardgame' :
-          imgTag =  `<img src="/_nuxt/img/6261fa6.PNG" class="lg:object-scale-down max-h-small"/>`
-          break
+    mapImg(sportType) {
+      var imgTag =
+        '<img src="/_nuxt/img/53a1fad.png" class="lg:object-scale-down max-h-small"/>';
+      switch (sportType) {
+        case "Badminton":
+          imgTag = `<img src="/_nuxt/img/241e671.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Basketball":
+          imgTag = `<img src="/_nuxt/img/11dd02b.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "E-sport":
+          imgTag = `<img src="/_nuxt/img/fde2e01.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Football":
+          imgTag = `<img src="/_nuxt/img/33dc3c6.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Futsal":
+          imgTag = `<img src="/_nuxt/img/8578cbc.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Petanque":
+          imgTag = `<img src="/_nuxt/img/8ad1bed.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Tabletennis":
+          imgTag = `<img src="/_nuxt/img/18b415f.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Rugbyfootball":
+          imgTag = `<img src="/_nuxt/img/bcb9655.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Athletics":
+          imgTag = `<img src="/_nuxt/img/c61456b.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Entertainment":
+          imgTag = `<img src="/_nuxt/img/add23ac.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Sepaktakraw":
+          imgTag = `<img src="/_nuxt/assets/02b4701.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Softball":
+          imgTag = `<img src="/_nuxt/img/0857216.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Swimming":
+          imgTag = `<img src="/_nuxt/img/cdaf21d.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Tennis":
+          imgTag = `<img src="/_nuxt/img/3702de5.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Volleyball":
+          imgTag = `<img src="/_nuxt/img/e6754dd.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
+        case "Boardgame":
+          imgTag = `<img src="/_nuxt/img/6261fa6.PNG" class="lg:object-scale-down max-h-small"/>`;
+          break;
       }
-      return imgTag
+      return imgTag;
     }
   },
 
-  async mounted(){
-    const res = await api.get('/api/incomingMatch')
-    const arr = await this.getIncomingMatch(res.data)
-    await this.appendincomingMatch(arr)
+  async mounted() {
+    const res = await api.get("/api/incomingMatch");
+    const arr = await this.getIncomingMatch(res.data);
+    await this.appendincomingMatch(arr);
   }
-
 };
 </script>
 
