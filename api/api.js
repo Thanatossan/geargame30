d21efafb248ae0b9f6d98baf73f774da100ebbb7
Thanatossan <kotokikung@gmail.com>
2019-12-26 11:32:38 +0700

    fix api

--- api/api.js
+++ api/api.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
-  withCredentials: true,
+  baseURL: "https://jhanglek.eng.cmu.ac.th",
+  withCredentials: false,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json"
